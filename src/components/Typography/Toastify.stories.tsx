49d732226708d5e00db14b792f019258318ea4cd
Aimazing <aimazingfeed@gmail.com>
2022-04-20 13:57:43 +0300

    Typography component was updated

--- src/components/Typography/Toastify.stories.tsx
+++ src/components/Typography/Toastify.stories.tsx
@@ -10,12 +10,11 @@ export default {
 
 export const Default: React.FC = () => (
   <>
-    <H1>H1 Headline 64 px | inter</H1>
-    <H2>H2 Headline 32 px | inter</H2>
-    <H3>H3 Headline 20 px | inter</H3>
-    <Text size="l">Body L 18 px | inter</Text>
-    <Text size="m">Body M 16 px | inter</Text>
-    <Text size="s">Body S 14 px | inter</Text>
-    <Text size="xs">Caption 12 px | inter</Text>
+    <H1 weight="bold">H1 Headline 48 px | inter | Bold</H1>
+    <H2 weight="semiBold">H2 Headline 32 px | inter | Semibold</H2>
+    <H3>H3 Headline 24 px | inter </H3>
+    <Text>Body S 14 px | inter </Text>
+    <Text size="m">Body M 16 px | inter </Text>
+    <Text size="l">Body L 18 px | inter </Text>
   </>
 );
