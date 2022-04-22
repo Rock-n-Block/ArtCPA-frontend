0e8f06cd32d4b82c702f1ad210299c2de7009b93
StanislavBokov <90760754+StanislavBokov@users.noreply.github.com>
2022-04-22 18:20:42 +0300

    Feature/cpa 9 (#6)
    * change component btns
    
    * create FollowUs
    
    * bugfix FollowUs
    
    * bugfix
    
    * add atributs for anchor

--- src/assets/icons/icons/components/index.ts
+++ src/assets/icons/icons/components/index.ts
@@ -2,4 +2,7 @@ export * from './ToastifyIcons';
 export * from './Tokens';
 export { StarIcon } from './StarIcon';
 export { ChevronDown } from './ChevronDown';
+export { Twitter } from './Twitter/Twitter';
+export { Telegram } from './Telegram/Telegram';
+export { Discord } from './Discord/Discord';
 export { MainLogoIcon } from './MainLogo';
