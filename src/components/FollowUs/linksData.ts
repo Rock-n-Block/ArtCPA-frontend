ef4ad60bde9f59faa555c6dba8bae1e28b2350ee
StanislavBudin <zerrozauros96@list.ru>
2022-04-22 17:11:07 +0300

    bugfix

--- src/components/FollowUs/linksData.ts
+++ src/components/FollowUs/linksData.ts
@@ -2,16 +2,19 @@ import { Twitter, Telegram, Discord } from 'assets/icons/icons';
 
 export const linksData = [
   {
+    id: 1,
     href: 'https://twitter.com/playmetarising',
     icon: Twitter,
     name: 'Twitter',
   },
   {
+    id: 2,
     href: 'https://t.me/metarising',
     icon: Telegram,
     name: 'Telegram Channel',
   },
   {
+    id: 3,
     href: 'https://discord.gg/metarising',
     icon: Discord,
     name: 'Discord',
