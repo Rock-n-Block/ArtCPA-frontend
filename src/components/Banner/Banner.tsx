3fc1b4f3ed3abee23ea237afb51e2d505fa6631e
Aimazing <aimazingfeed@gmail.com>
2022-04-22 18:44:58 +0300

    Fixed the edits

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,6 @@
 import { VFC } from 'react';
 
 import cn from 'clsx';
-// import { BannerBackgroundImg } from 'assets/img/icons';
 import { H1 } from 'components/Typography';
 import { Button } from 'components/Button';
 import styles from './styles.module.scss';
@@ -14,7 +13,6 @@ export interface BannerProps {
 export const Banner: VFC<BannerProps> = ({ className, onButtonClick }) => {
   return (
     <div className={cn(styles.banner, className)}>
-      {/* <img src={BannerBackgroundImg} alt="" /> */}
       <div className={styles.container}>
         <H1 align="center" className={styles.bannerText}>
           The place where Community, Art, AI,
@@ -23,7 +21,6 @@ export const Banner: VFC<BannerProps> = ({ className, onButtonClick }) => {
         </H1>
         <Button variant="filled" size="md" onClick={onButtonClick}> BUY CPA </Button>
       </div>
-
     </div>
   );
 };
