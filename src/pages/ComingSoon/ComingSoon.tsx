a60004e92617de90cffb39c1a9a813b20e23d8f3
Aimazing <aimazingfeed@gmail.com>
2022-04-25 15:28:09 +0300

    Fixed the edits

--- src/pages/ComingSoon/ComingSoon.tsx
+++ src/pages/ComingSoon/ComingSoon.tsx
@@ -4,20 +4,19 @@ import cn from 'clsx';
 import { Text, H1 } from 'components/Typography';
 
 import { Button } from 'components/Button';
-// import { Header } from 'containers';
+import { useNavigate } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 export interface ComingSoonProps {
   className?: string;
   title: string;
-  onButtonClick: () => void;
 }
 
-export const ComingSoon: VFC<ComingSoonProps> = ({ className, title, onButtonClick }) => {
+export const ComingSoon: VFC<ComingSoonProps> = ({ className, title }) => {
+  const navigate = useNavigate();
+  const onButtonClick = () => navigate('/');
   return (
     <div className={cn(styles.comingSoon, className)}>
-      {/* <Header chainType="testnet" address="" onToggleChainType={() => {}} onConnectWallet={() => {}} disconnect={() => {}} isHomePage isUserInfoLoading /> */}
-      <H1 className={styles.header}>Header</H1>
       <div className={styles.container}>
         <H1 weight="bold" align="center" className={styles.title}>{title}</H1>
         <Text weight="bold" className={styles.bodyText} align="center"> Coming soon </Text>
