57f2b1149321a284e6b3611dc2ced3b99bfc8187
StanislavBudin <zerrozauros96@list.ru>
2022-04-25 17:19:27 +0300

    bugfix

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -100,7 +100,6 @@ export const Input:VFC<InputProps> = ({
             className={cn(
               styles.labelText,
               classNameLabel,
-              // { [styles.noneLabel]: value },
             )}
           >
             {label}
