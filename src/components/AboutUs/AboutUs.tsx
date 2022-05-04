c4ad3ed8ed4e5c95d648000fa085a53c546d85a8
StanislavBudin <zerrozauros96@list.ru>
2022-05-04 10:50:14 +0300

    change font-weigh for components

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -18,8 +18,8 @@ export const AboutUs: VFC<AboutUsProps> = ({ className }) => {
       <div className={styles.wrapperCard}>
         {aboutUsHelper.map((card) => (
           <Card size="sm" className={styles.cardItem} key={card.id}>
-            <H2 className={styles.title}>{card.title}</H2>
-            <Text size="m" className={styles.description} noWrap={false}>{card.description}</Text>
+            <H2 className={styles.title} weight="semiBold">{card.title}</H2>
+            <Text size="m" className={styles.description} noWrap={false} weight="light">{card.description}</Text>
           </Card>
         ))}
       </div>
